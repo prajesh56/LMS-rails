8b9ef7cd6b5a303b502ba2af4b3766110cb97512
prajesh56 <pj.shakya10@gmail.com>
2022-02-25 12:46:23 +0545

    Homepage login successful

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+	def dashboard
+		if session[:employee_id]
+			@user = Employee.find(session[:employee_id])
+			if @user.role == false
+				@employees = Employee.where.not(id: @user.id)
+			end
+		end
+	end
+	def create
+		@employee = Employee.find_by(email: params[:email])
+      if @employee.present? && @employee.password == params[:password]
+          session[:employee_id] = @employee.id
+          redirect_to '/dashboard', notice: "logged in sucessfully"
+      else
+          flash[:notice]= "invalid email or password"
+          redirect_to root_path
+      end
+	end
+
+	def destroy
+		session[:employee_id] = nil
+		redirect_to root_path, notice: "logged out"
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
   resource :attendence
   resource :employee
   resource :leave
-  
+  resource :home
+
+  get "/", to: "home#index"
+  post "sign_in", to: "home#create"
+  get "dashboard", to:"home#dashboard"
+  delete "logout", to: "home#destroy"
 end
