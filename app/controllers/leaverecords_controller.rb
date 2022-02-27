c9329c6442db93314d606f3308c68babc106756a
prajesh56 <pj.shakya10@gmail.com>
2022-02-27 23:15:42 +0545

    1. Personal leave request display successful 2. Admin can change status of leave 3. once status of leave is changed into accepted or rejected edit button is removed 4. Only pending leave can be deleted by employees

--- app/controllers/leaverecords_controller.rb
+++ app/controllers/leaverecords_controller.rb
@@ -1,10 +1,15 @@
 class LeaverecordsController < ApplicationController
   def index
+		#@leaverecords = Leaverecord.where.not(employee_id: session[:employee_id])
 		@leaverecords = Leaverecord.all
 	end
 
 	def new
 		@leaverecords = Leaverecord.new
+	end
+
+	def show
+		@leaverecords = Leaverecord.where(employee_id: params[:id])
 	end
 
 	def edit
@@ -13,7 +18,7 @@ class LeaverecordsController < ApplicationController
 
 	def update
 		@leaverecords = Leaverecord.find(params[:id])
-	  if @leaverecords.update(leaverecord_params)
+	  if @leaverecords.update(edit_leaverecord_params)
       redirect_to leaverecords_path
     else
       render :edit, status: :unprocessable_entity
@@ -21,7 +26,7 @@ class LeaverecordsController < ApplicationController
 	end
 
 	def create
-		@leaverecords = Leaverecord.new(leaverecord_params)
+		@leaverecords = Leaverecord.new(new_leaverecord_params)
 		if @leaverecords.save
 			redirect_to dashboard_path, notice: "New Leave request created successfully" 
 		else
@@ -29,13 +34,19 @@ class LeaverecordsController < ApplicationController
 		end
 	end
 
+	def destroy
+		leaverecords = Leaverecord.find(params[:id])
+		leaverecords.destroy
+		redirect_to dashboard_path
+	end
+
 	private
-	def leaverecord_params
-		if session[:employee_role]== 'admin'
-			params.require(:leaverecord).permit(:id, :date_from, :date_to, :description, :employee_id, :status, :approval_by)
-		else
-			params.require(:leaverecord).permit(:id, :date_from, :date_to, :description, :employee_id, :status)
-		end
+	def edit_leaverecord_params
+		params.require(:leaverecord).permit(:status, :approval_by)
+	end
+
+	def new_leaverecord_params
+		params.require(:leaverecord).permit(:id, :date_from, :date_to, :description, :employee_id, :status)
 	end
 	
 end
