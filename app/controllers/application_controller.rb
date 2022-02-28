e2691aadb51263563e2b3b25a06af8b96bcdbe88
prajesh56 <pj.shakya10@gmail.com>
2022-02-28 20:37:13 +0545

    Header and nav bar loaded in application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    return unless session[:employee_id]
+    @current_user ||= Employee.find(session[:employee_id])
+  end
 end
