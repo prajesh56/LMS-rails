083340a16b5c9b4787417ed10e12931e3b5b2002
prajesh56 <pj.shakya10@gmail.com>
2022-07-12 23:23:44 +0545

    Fix: fixes made for buttons and background image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,18 +15,18 @@ class HomeController < ApplicationController
 		@users = User.where.not(id: current_user.id).order(created_at: :desc)
 	end
 
-	def create
-		@employee = Employee.find_by(email: params[:email])
-		if @employee.present? && @employee.password == params[:password]
-		  session[:employee_id] = @employee.id
-			session[:employee_name] = @employee.name 
-			session[:employee_role] = @employee.role 
-		  redirect_to '/dashboard', notice: "logged in sucessfully"
-		else
-		  flash[:notice]= "invalid email or password"
-		  redirect_to root_path
-		end
-	end
+	#def create
+	#	@employee = Employee.find_by(email: params[:email])
+	#	if @employee.present? && @employee.password == params[:password]
+	#	  session[:employee_id] = @employee.id
+	#		session[:employee_name] = @employee.name 
+	#		session[:employee_role] = @employee.role 
+	#	  redirect_to '/dashboard', notice: "logged in sucessfully"
+	#	else
+	#	  flash[:notice]= "invalid email or password"
+	#	  redirect_to root_path
+	#	end
+	#end
 
 	
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "prajesh.shakya12@gmail.com"
   layout "mailer"
 end
